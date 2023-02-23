0e49def577e7a0f1dadd3695e879f1738c54d9a4
Xavier Galver <xaviergalver@gmail.com>
2023-02-23 18:12:36 +0100

    labs done

---
+++ 00-Labs/lab-vue-c-intro/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"> 
+    <img src="../src/images/Vue Lab - Welcome to Vue.js.png" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style>
+  #app {
+  }
+</style>
