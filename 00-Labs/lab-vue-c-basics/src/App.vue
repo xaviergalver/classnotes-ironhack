91d458a469016241f7887b5c20bfb542eca3fb88
Xavier Galver <xaviergalver@gmail.com>
2023-02-23 18:35:48 +0100

    labs done

---
+++ 00-Labs/lab-vue-c-basics/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavComponent from "./components/NavComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import Iteration2 from "./components/Itineration2.vue";
+import Iteration3 from "./components/Itineration3.vue";
+</script>
+
+<template>
+  <NavComponent />
+  <Iteration2 />
+  <Iteration3 />
+  <FooterComponent />
+</template>
+
+<style scoped></style>
